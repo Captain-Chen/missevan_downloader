8e0268b5dd0286accceebcb13cba1fe1d1b3a27e
Richard Nguyen <almachy@gmail.com>
2025-03-17 18:26:42 -0500

    initial working version

---
+++ README.md
@@ -0,0 +1,48 @@
+# MissEvan Downloader | 猫耳FM广播剧下载工具
+
+## What is this?
+A tool that can be used to download all of your favorite radio dramas from MissEvan/猫耳FM. 
+
+### Features
+* Fast concurrent downloads using a single session
+* Can handle large audio files without using a lot of memory
+* Infers the file extension from the HTTP response header
+
+## How to install (on Windows)
+1. Clone the project:  
+`git clone https://github.com/Captain-Chen/missevan_downloader.git`
+2. Install the dependencies: `python -m pip install -r requirements.txt`
+3. Decide whether you want to run the script directly or compile as a standlone executable.
+    * To run the script directly: `python missevan_downloader.py`  
+    * To compile as a standalone executable:
+        * Install PyInstaller: `python -m pip install pyinstaller`
+        * Run: `pyinstaller missevan_downloader.py --onefile --windowed && cd dist`
+        * Run `missevan_downloader.exe` inside the `dist` folder and follow the prompts. You may run this executable from anywhere on your machine.
+
+## How to use (on Windows)
+> **Optional but recommended step:**
+
+Some radio dramas are paid and require you to own them in order to download.
+  * If you have a registered account on MissEvan/猫耳FM:
+    * Create a `token.txt` file in the same folder as `missevan_downloader.py` or the bundled application.
+  * Locate and copy your token from your browser:  
+  `Developer Tools > Storage > Cookies > https://www.missevan.com > token > somelongvalue`
+  * Paste your token value into `token.txt` and save.
+
+> **DO NOT SHARE YOUR TOKEN WITH ANYONE ELSE**
+
+1. Run the script directly: `python missevan_downloader.py` or the bundled executable.
+2. Enter the url of the drama or episode.  
+e.g. `https://www.missevan.com/mdrama/drama/{someId}`  
+`https://www.missevan.com/sound/player?id={someId}`
+3. The program will look for the corresponding audio files and download them into the `dl` folder. If the file cannot be downloaded it will be skipped.
+4. If the downloads fail, your token may be incorrect or expired. Replace the value inside `token.txt` with the new token value.
+
+*Note:* Code may be unoptimized and contain bugs.
+
+### Dependencies & Requirements
+Requires Python **v3.7+**
+* [asyncio](https://docs.python.org/3/library/asyncio.html)
+* [aiohttp](https://docs.aiohttp.org/en/stable/)
+* [aiofiles](https://pypi.org/project/aiofiles/)
+* [validators](https://validators.readthedocs.io/en/latest/)
---
+++ missevan_downloader.py
@@ -0,0 +1,108 @@
+import time
+import os
+import re
+import sys
+from mimetypes import guess_extension
+
+import asyncio
+import aiofiles
+import validators
+from aiohttp import ClientSession
+
+base_url = "https://www.missevan.com"
+
+if getattr(sys, 'frozen', False):
+    app_path = os.path.abspath('.')
+else:
+    app_path = os.path.dirname(__file__)
+
+download_folder = os.path.join(app_path, 'dl')
+
+if not os.path.exists(download_folder):
+    os.mkdir(download_folder)
+
+def is_drama_link(url):
+    if "drama" in url:
+        return True
+    return False
+
+invalid_characters = r"^[ .]|[/<>:\"\\|?*]+|[ .]$"
+def sanitize_filename(filename):
+    """Replaces any invalid characters"""
+    return re.sub(invalid_characters, "_", filename) 
+
+async def fetch_token():
+    token_path = os.path.join(app_path, 'token.txt')
+    if not os.path.exists(token_path):
+        return None
+    
+    async with aiofiles.open(token_path, 'r') as f:
+        token_value = await f.read()
+
+    return {"Cookie": f"token={token_value}"}
+
+async def download_audio(id, session, **kwargs):
+    url = f"{base_url}/sound/getsound?soundid={id}"
+    resp = await session.request(method="GET", url=url, **kwargs)
+    resp.raise_for_status()
+
+    json_content = await resp.json()
+
+    sound_title = sanitize_filename(json_content['info']['sound']['soundstr']) 
+    sound_url = json_content['info']['sound'].get('soundurl')
+    if sound_url is None:
+        print(f"Unable to download file for {sound_title}")
+        return
+
+    resp = await session.request(method="GET", url=sound_url)
+    file_extension = guess_extension(resp.content_type)
+    if file_extension is not None:
+        file_name = f"{sound_title}{file_extension}"
+    else:
+        file_name = sound_title
+
+    download_path = os.path.join(download_folder, file_name)
+    chunk_size = 1024 * 1024 * 1024 * 4 # 4 GiB
+    async with aiofiles.open(download_path, 'wb') as f:
+        async for chunk in resp.content.iter_chunked(chunk_size):
+            await f.write(chunk)
+    print(f"Download finished for {download_path}")
+
+async def fetch_audio_list(drama_id, session, **kwargs):
+    url = f"{base_url}/dramaapi/getdrama?drama_id={drama_id}"
+    resp = await session.request(method="GET", url=url, **kwargs)
+    resp.raise_for_status()
+    
+    json_content = await resp.json()
+    audio_ids = set()
+    for episode in json_content['info']['episodes']['episode']:
+        if episode['sound_id'] not in audio_ids:
+            audio_ids.add(episode['sound_id'])
+    return audio_ids
+
+async def main():
+    url = input("Please paste the full url here: ")
+    if not validators.url(url):
+        return
+    headers = await fetch_token()
+    async with ClientSession(headers=headers) as session:
+        if not is_drama_link(url):
+            audio_id = url.split('=')[-1]
+            await download_audio(audio_id, session)
+        else:
+            drama_id = url.split('/')[-1]
+            audio_list = await fetch_audio_list(drama_id, session)
+            tasks = []
+            for audio_id in audio_list:
+                tasks.append(
+                    download_audio(audio_id, session)
+                )
+            await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
+
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"Job took {elapsed:.2f} seconds.")
